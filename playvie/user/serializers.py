03b2c511dc856e0f28aaa4ff73069945a24b4c10
andrscc <andrescc143@gmail.com>
2023-03-26 16:55:10 -0500

    Some comments were added to the serializer.py file, in the User app, to make the code easier to understand

--- playvie/user/serializers.py
+++ playvie/user/serializers.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+#The serializer are related through the HyperlinkedModel, which adds URL to access the related data from the other table/entity
 
+#To represent the data related to each user
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
 
-
+#To represent the data from each Group of users
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
