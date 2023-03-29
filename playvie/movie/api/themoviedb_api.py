c8b9bf7cdd55bda63f0f6f99f139e3bb563d5690
andrscc <andrescc143@gmail.com>
2023-03-28 19:58:05 -0500

    The TheMovieDB API functions were create to retrieve the data from the movie/popular and /genre/movie/list endpoints

---
+++ playvie/movie/api/themoviedb_api.py
@@ -0,0 +1,24 @@
+import requests
+
+ 
+base_url = url = 'https://api.themoviedb.org/3'
+
+def get_genre_data(api_key):
+    endpoint = '/genre/movie/list'
+    url = base_url + endpoint + f'?api_key={api_key}'    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def get_movie_data(api_key):
+    endpoint = '/movie/popular'
+    url = base_url + endpoint + f'?api_key={api_key}'    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
