f99780c9fdeef783db3b01c43484f303f6aa8ab5
andrscc <andrescc143@gmail.com>
2023-03-28 19:56:47 -0500

    The database connection was configure to use a MySQL DB and the corresponding migration was executed

--- playvie/playvie/settings.py
+++ playvie/playvie/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+THEMOVIE_API_KEY = '2212bc4709e02d211e84ffa8614e8c53'
+
 INSTALLED_APPS = [
     'rest_framework',
     'django.contrib.admin',
@@ -82,8 +84,12 @@ WSGI_APPLICATION = 'playvie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'playvie',
+        'USER': 'root',
+        'PASSWORD': 'Root@password',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
