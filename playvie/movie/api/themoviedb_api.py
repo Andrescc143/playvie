a361914cdf112bd8508b504a2c33c9e36921c473
andrscc <andrescc143@gmail.com>
2023-04-05 18:20:09 -0500

    The function to interact with the API was modified to receive the specific page of data the user wants to retrieve

--- playvie/movie/api/themoviedb_api.py
+++ playvie/movie/api/themoviedb_api.py
@@ -13,9 +13,13 @@ def get_genre_data(api_key):
     else:
         return None
 
-def get_movie_data(api_key):
+def get_movie_data(api_key, page=None):
     endpoint = '/movie/popular'
-    url = base_url + endpoint + f'?api_key={api_key}'    
+    url = base_url + endpoint + f'?api_key={api_key}'
+    
+    if page:
+        url = url + f'&page={page}'
+          
     response = requests.get(url)
     
     if response.status_code == 200:
