6efada98689bec8c480993afb76332b03bc1925b
andrscc <andrescc143@gmail.com>
2023-04-07 15:43:50 -0500

    The urls for the movie app endpoints were change to delete the 'source' part added

--- playvie/movie/urls.py
+++ playvie/movie/urls.py
@@ -4,6 +4,5 @@ from movie.views import get_movies_view, get_genres_view
 
 urlpatterns = [
     path('', get_movies_view, name='movies_api'),
-    path('<int:page>', get_movies_view, name='movies_api'),
     path('genres', get_genres_view, name='genres_api')
 ]
