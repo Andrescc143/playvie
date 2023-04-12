8432b086819b563704994a2d3f6c08e7ddea6bbb
andrscc <andrescc143@gmail.com>
2023-04-11 22:53:28 -0500

    The viewset to perform all the CRUD operations over the PLaylist model was created and the create method was customized to display a specific message

--- playvie/movie/views.py
+++ playvie/movie/views.py
@@ -136,3 +136,13 @@ def get_genres_view(request):
 class PlaylistViewSet(ModelViewSet):
     queryset = Playlist.objects.all().order_by('id')
     serializer_class = PlaylistSerializer
+    
+    def create(self, request):                     
+        serialized_playlist = self.serializer_class(data=request.data)
+        
+        if serialized_playlist.is_valid():
+            serialized_playlist.save()                
+            return Response({'message': 'Playlist created correctly', 'data': serialized_playlist.data}, status=status.HTTP_201_CREATED)
+        
+        return Response(serialized_playlist.errors, status=status.HTTP_400_BAD_REQUEST)
+        
