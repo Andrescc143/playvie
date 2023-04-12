217e5884acc20e676796ba8fb7a4ff2fc0559a1f
andrscc <andrescc143@gmail.com>
2023-04-12 13:46:10 -0500

    The constant values like the base URL for the poster of each movie, or the API key, were moved to an independent faile called CONSTANTS, inside the root project folder 'playvie'

---
+++ playvie/playvie/CONSTANTS.py
@@ -0,0 +1,3 @@
+BASE_URL_MOVIE_POSTER = 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2'
+API_KEY = '2212bc4709e02d211e84ffa8614e8c53'
+GENRE_IDS ={28: 'Action', 12: 'Adventure', 16: 'Animation', 35: 'Comedy', 80: 'Crime', 99: 'Documentary', 18: 'Drama', 10751: 'Family', 14: 'Fantasy', 36: 'History', 27: 'Horror', 10402: 'Music', 9648: 'Mystery', 10749: 'Romance', 878: 'Science Fiction', 10770: 'TV Movie', 53: 'Thriller', 10752: 'War', 37: 'Western'}
