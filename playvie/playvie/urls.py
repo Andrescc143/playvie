ba8c95a6fd16ccae0c65fd48153384ed31b0585f
andrscc <andrescc143@gmail.com>
2023-07-19 23:22:55 -0500

    Feature: The JWT authentication was implemented;The Simple JWT package was added and configure to the app;A custom view for the token generation was created to add the username to the info encrypted; Rotate and blacklist functions were activated

--- playvie/playvie/urls.py
+++ playvie/playvie/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from user.views import UserViewSet, GroupViewSet
-
 from playvie.views import handler404
+
+
 
 
 router = routers.DefaultRouter()
@@ -18,8 +20,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/movies/', include('movie.urls')),
-    path('api/v1/playlists/', include('playlist.urls'))
+    path('api/v1/playlists/', include('playlist.urls')),
 ]
 
 handler500 = 'rest_framework.exceptions.server_error'
--- playvie/playvie/views.py
+++ playvie/playvie/views.py
@@ -1,5 +1,17 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from django.http import JsonResponse
 
-
+# ERROR HANDLING
 def handler404(request, exception=None):
     return JsonResponse({'error': 'Resource Not Found (404)'}, status=404)
+
+#AUTHENTICATION
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['name'] = user.username
+
+        return token
